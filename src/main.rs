49c56a33bd5136304d9419b0a5a8390589b6be21
Cheethas <47148561+cheethas@users.noreply.github.com>
2022-10-28 23:13:00 +0100

    feat: lex macro bodies

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,106 @@ use chumsky_huff::opcodes::{Opcode, OPCODES, OPCODES_MAP};
 
 #[derive(Debug, Clone)]
 enum Token {
+    // Literals
+    /// Hex literal represents 256 bit value
     HexLiteral(String),
+
+    /// An opcode represents a valid evm opcode
     Opcode(Opcode),
-    Newline,
+
+    /// Represents a Jump Label
     JumpLabel(String),
+
+    Macro {
+        name: String,
+        takes: u32,
+        returns: u32,
+        args: Vec<String>,
+        body: Vec<Token>,
+    },
+
+    Newline,
+
+    Error,
 }
+
+/// Error strategies
+// skip_then_retry_with();
+// skip_then_retry_until
+// skip_until
+// nested_delimiters
 
 // Create a token mapping of keyword to opcode
 
 fn parser() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
+    lex_macro().repeated()
+}
+
+/// Lex Macro
+///
+/// Steps:
+///     1. Find `#define` keyword
+///     2. the macro keyword
+///     3. the macro arguments inside ( )
+///     4. find equals
+///     5. Find the takes() value, default to 0
+///     6. Find the returns() value, default to 0
+///     7. Find the macro body
+///
+/// TODO: right now do not allow nested macros, that will come later
+fn lex_macro() -> impl Parser<char, Token, Error = Simple<char>> {
+    // Pad keyword finders
+    let ident = text::ident().padded();
+    let key = |c| text::keyword(c).padded();
+    let char = |c| just(c).padded();
+
+    // Other lexers
+    let macro_body = lex_macro_body();
+
+    just('#')
+        .ignore_then(key("define"))
+        .ignore_then(key("macro"))
+        .ignore_then(ident)
+        .then_ignore(char('='))
+        // TODO: turn takes into its own lex so the whole thing can be if or
+        .then_ignore(key("takes"))
+        .then_ignore(char('('))
+        .then(text::digits(10).or_not())
+        .then_ignore(char(')'))
+        // TODO: turn returns into its own lex so the whole thing can be if or
+        .then_ignore(key("returns"))
+        .then_ignore(char('('))
+        .then(text::digits(10).or_not())
+        .then_ignore(char(')'))
+        .then_ignore(char('{'))
+        .then(macro_body)
+        .then_ignore(char('}'))
+        .map(|(((name, takes), returns), macros)| {
+            // println!("{name} {:?}", name = name, takes);
+            Token::Macro {
+                name: name,
+                // TODO: clean up this line
+                takes: takes.unwrap_or(0.to_string()).parse().unwrap(),
+                returns: returns.unwrap_or(0.to_string()).parse().unwrap(),
+                args: vec![],
+                body: macros,
+            }
+        })
+        .padded()
+        .labelled("macro_body")
+}
+
+fn lex_macro_body() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
     let newline = lex_newline_and_comments();
     let opcode = lex_opcode_or_jump_label();
     let hex_literal = lex_hex_number();
 
-    opcode.or(hex_literal).or(newline.clone()).repeated()
+    opcode
+        .or(hex_literal)
+        .or(newline.clone())
+        // TODO: come up with better error handling strategies
+        .recover_with(skip_then_retry_until([' ']))
+        .repeated()
 }
 
 fn lex_hex_number() -> impl Parser<char, Token, Error = Simple<char>> {
